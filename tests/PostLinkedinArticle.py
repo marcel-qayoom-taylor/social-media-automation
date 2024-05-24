0d47a149e51067319c9b48db7de902bdf8d57b20
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-05-25 01:11:58 +1000

    working persistent auth

--- tests/PostLinkedinArticle.py
+++ tests/PostLinkedinArticle.py
@@ -1,15 +1,27 @@
 import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 
-
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
-    page.goto("https://www.linkedin.com/login")
-    page.get_by_label("Email or phone").fill("dion@100doors.co")
-    page.get_by_label("Password", exact=True).fill("Paula$1977")
-    page.get_by_label("Sign in", exact=True).click()
+    try: 
+        
+        context = browser.new_context(storage_state="..\playwright\.auth\linkedinAuthState.json")
+        page = context.new_page()
+        page.goto("https://www.linkedin.com/feed/")
+        print("Loaded existing storage state from linkedinAuthState.json")
+    except FileNotFoundError:
+        print("Could not find existing auth state from linkedinAuthState.json")
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto("https://www.linkedin.com/login")
+        page.get_by_label("Email or phone").fill("dion@100doors.co")
+        page.get_by_label("Password", exact=True).fill("Paula$1977")
+        page.get_by_label("Sign in", exact=True).click()
+        
+        # Save storage state for future use
+        context.storage_state(path="..\playwright\.auth\linkedinAuthState.json")
+        print("Saved storage state to state.json")
+
     page.get_by_label("Write an article on LinkedIn").click()
     page.get_by_role("radio", name="Dion Guagliardo").click()
     page.get_by_role("button", name="Next").click()
