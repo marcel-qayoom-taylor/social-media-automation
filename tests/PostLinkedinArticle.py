182872205c7b9a5d9019ea67cc998292f156e64a
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-05-27 23:26:12 +1000

    fix race condition and image upload bug

--- tests/PostLinkedinArticle.py
+++ tests/PostLinkedinArticle.py
@@ -1,5 +1,6 @@
 import re
 import os
+import asyncio
 from playwright.sync_api import Playwright, sync_playwright, expect
 from dotenv import load_dotenv
 
@@ -9,17 +10,20 @@ def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
     try: 
         context = browser.new_context(storage_state="..\playwright\.auth\linkedinAuthState.json")
+        
+        # Open linkedin using saved credentials
         page = context.new_page()
         page.goto("https://www.linkedin.com/feed/")
         print("Loaded existing storage state from linkedinAuthState.json")
-    except FileNotFoundError:
+    except FileNotFoundError: # if no credentials cached
         print("Could not find existing auth state from linkedinAuthState.json")
         linkedinUsername = os.getenv("LINKEDIN_USERNAME")
         linkedinPassword = os.getenv("LINKEDIN_PASSWORD")
         if not linkedinUsername or not linkedinPassword:
             print("Error: Missing LinkedIn Credentials! Please ensure both 'linkedinUsername' and 'linkedinPassword' environment variables are set in your '.env' file.")
             return
-
+        
+        # Login to LinkedIn
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://www.linkedin.com/login")
@@ -31,19 +35,23 @@ def run(playwright: Playwright) -> None:
         context.storage_state(path="..\playwright\.auth\linkedinAuthState.json")
         print("Saved storage state to state.json")
 
+    # Write article content
     page.get_by_label("Write an article on LinkedIn").click()
     page.get_by_role("radio", name="Dion Guagliardo").click()
     page.get_by_role("button", name="Next").click()
-    page.get_by_placeholder("Title").fill("Test Title")
-    page.get_by_label("Write here. You can also").click()
-    page.get_by_label("Article editor content").fill("test content")
-    # page.get_by_role("group", name="We recommend uploading or").locator("label").click()
-    # page.get_by_label("Upload from computer").set_input_files("COOKING APP (1).png")
-    page.get_by_label("Article editor content").press("Tab")    
-    page.get_by_role("button", name="Next").click()
-    page.get_by_role("paragraph").click()
-    page.get_by_label("Text editor for creating").fill("Test intro para")
+    page.get_by_placeholder("Title").click()
+    page.get_by_placeholder("Title").press_sequentially("Test Title")
+    expect(page).to_have_url(re.compile(".*article/edit/"))
+    page.get_by_label("Article editor content").click()
+    page.get_by_label("Article editor content").press_sequentially("Test intro para")
 
+    # Add image
+    page.get_by_label("Upload from computer").set_input_files("slideshow.png") # image has to be in directory or use file picker but needs async
+    
+    # Move to publish page
+    page.get_by_role("button", name="Next").click(delay=2000) # wait for draft to save
+    page.get_by_label("Text editor for creating").click()
+    page.get_by_label("Text editor for creating").press_sequentially("Test intro para")
 
     # ---------------------
     context.close()
