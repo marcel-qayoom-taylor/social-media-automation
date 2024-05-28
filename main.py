77c65b2cbd1f55c17446b7030bebdaf26f31e171
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-05-28 23:41:39 +1000

    fonts and disclaimers

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+# Fonts
+font_heading = ("Arial", 13, "bold")
+font_subheading = ("Arial", 10, "")
+
 def submit_data():
   # Get the data from the entry field
   data = entry.get()
@@ -8,58 +12,60 @@ def submit_data():
 
 # Create the main window
 window = Tk()
-window.title("Input Form")
+window.title("Social Media Poster")
 window.grid_columnconfigure(0, weight=1, minsize=75)
 window.grid_columnconfigure(1, weight=1, minsize=200)
 
-# Article Frame
+# Article Frame -------------------------------------------------------------
 frm_article = Frame(master=window, padx=10, pady=5)
+lbl_frame_title = Label(master=frm_article, text="Article Details", font=font_heading)
 
-# Article Title Frame
-frm_article_title = Frame(master=frm_article, pady=10)
-  # Title label and entry
-lbl_title = Label(master=frm_article_title, text="Enter article title:")
-ent_title = Entry(master=frm_article_title, width=50)
+# Title label and entry
+lbl_title = Label(master=frm_article, text="Enter article title:", font=font_subheading)
+ent_title = Entry(master=frm_article, width=50)
 
-# Article Body Frame
-frm_article_body = Frame(master=frm_article, pady=10)
-  # Body label and text box
-lbl_body = Label(master=frm_article_body, text="Enter article body:")
-txt_body = Text(master=frm_article_body, width=70, height=20)
+# Body label and text box
+lbl_body = Label(master=frm_article, text="Enter article body:", font=font_subheading)
+txt_body = Text(master=frm_article, width=70, height=20)
 
-# Article Body Frame
-frm_article_intro = Frame(master=frm_article, pady=10)
-  # Body label and text box
-lbl_intro = Label(master=frm_article_intro, text="Enter article intro:")
-txt_intro = Text(master=frm_article_intro, width=70, height=10)
+# Body label and text box
+lbl_intro = Label(master=frm_article, text="Enter article intro:", font=font_subheading)
+txt_intro = Text(master=frm_article, width=70, height=10)
+
+# Disclaimer checkboxes
+frm_disclaimers = Frame(master=frm_article, pady=5)
+useGeneralDisclaimer = BooleanVar(value=True)
+useHistoricDisclaimer = BooleanVar(value=False)
+chk_general_disclaimer = Checkbutton(master=frm_disclaimers, text="Include general disclaimer", onvalue=True, offvalue=False, variable=useGeneralDisclaimer)
+chk_historic_disclaimer = Checkbutton(master=frm_disclaimers, text="Include historic disclaimer", onvalue=True, offvalue=False, variable=useHistoricDisclaimer)
 
 # Positioning
 frm_article.grid(row=0, column=0, padx=10, pady=5)
-lbl_title.grid(row=0, column=0, sticky='w')
-ent_title.grid(row=1, column=0)
-lbl_body.grid(row=2, column=0, sticky='w')
-txt_body.grid(row=3, column=0)
-lbl_intro.grid(row=4, column=0, sticky='w')
-txt_intro.grid(row=5, column=0)
+lbl_frame_title.grid(row=0, column=0, sticky='w', pady=5)
+lbl_title.grid(row=1, column=0, sticky='w', pady=5)
+ent_title.grid(row=2, column=0, sticky='w', pady=5)
+lbl_body.grid(row=3, column=0, sticky='w', pady=5)
+txt_body.grid(row=4, column=0, sticky='w', pady=5)
+lbl_intro.grid(row=5, column=0, sticky='w', pady=5)
+txt_intro.grid(row=6, column=0, sticky='w', pady=5)
+frm_disclaimers.grid(row=7, column=0, sticky='w')
+chk_general_disclaimer.grid(row=0, column=0, sticky='w')
+chk_historic_disclaimer.grid(row=0, column=1, sticky='w')
 
-frm_article_title.pack(fill=BOTH, expand=True)
-frm_article_body.pack(fill=BOTH, expand=True)
-frm_article_intro.pack(fill=BOTH, expand=True)
+# End of Article Frame -------------------------------------------------------------
 
-# Extras Frame
+# Extras Frame ---------------------------------------------------------------------
 frm_extras = Frame(master=window)
 frm_extras.grid(row=0, column=1, padx=10, pady=5)
 
-# Article Title Frame
-frm_extra_title = Frame(master=frm_extras, height=2, width=50)
-  # Title label and entry
-lbl_title2 = Label(master=frm_extra_title, text="Enter extra title:")
+# Title label and entry
+lbl_title2 = Label(master=frm_extras, text="Enter extra title:")
 lbl_title2.grid(row=0, column=1)
 
-ent_title2 = Entry(master=frm_extra_title)
+ent_title2 = Entry(master=frm_extras)
 ent_title2.grid(row=1, column=1)
+# End of Extras Frame ---------------------------------------------------------------------
 
-frm_extra_title.pack(fill=BOTH, expand=True)
 
 # Create the submit button
 btn_submit = Button(window, text="Submit", command=submit_data)
