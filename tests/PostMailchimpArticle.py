f139779eb84c68297d091972ea8854af753d0f3e
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-09-26 10:51:58 +1000

    write dion email for sender

--- tests/PostMailchimpArticle.py
+++ tests/PostMailchimpArticle.py
@@ -40,6 +40,10 @@ def postArticle(page):
     page.locator("label").filter(has_text="Send now").click()
     page.get_by_role("button", name="Save").click()
 
+    page.get_by_role("button", name="Edit from").click()
+    page.get_by_label("Email address").fill("dion@fortressfamilyoffice.com")
+    page.get_by_role("button", name="Save").click()
+    
     page.get_by_role("button", name="Edit design").click()
     page.frame_locator("iframe[title=\"Preview\"]").locator("#dataBlockId-5").get_by_role("paragraph").first.click()
     page.frame_locator("iframe[title=\"Preview\"]").locator("#dataBlockId-5").get_by_role("paragraph").first.fill(data['article']['title'] + '\n\n' + data['article']['body'])
